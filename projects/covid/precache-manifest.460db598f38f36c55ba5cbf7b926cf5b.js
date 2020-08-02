dac33fba27f1fc16444a56c3543bcbff9f529f1b
Cagatay Ucer <cagatay.ucer@gmail.com>
2020-08-02 18:58:24 +0300

    anasayfa degisti covid eklendi

---
+++ projects/covid/precache-manifest.460db598f38f36c55ba5cbf7b926cf5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28771c93f669f2a5c451aefdab907572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19ffb7ad02265f3e356a",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "247d20af389ac2426e45",
+    "url": "/static/css/main.1be3c61c.chunk.css"
+  },
+  {
+    "revision": "19ffb7ad02265f3e356a",
+    "url": "/static/js/2.31171797.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.31171797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247d20af389ac2426e45",
+    "url": "/static/js/main.ed7a4c99.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
