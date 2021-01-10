dcd8d91f514c711578def83d7f09f51d926b08fc
Cagatay Ucer <cagatay.ucer@gmail.com>
2021-01-10 17:40:06 +0300

    gh

---
+++ projects/covid/precache-manifest.2560818cbc4b3cd87b6c325c8b959e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395a454a9accf29d499a7937e27f8275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2cb6c84bd80f5869b6",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "03ada35c6262640fc403",
+    "url": "/static/css/main.1be3c61c.chunk.css"
+  },
+  {
+    "revision": "0e2cb6c84bd80f5869b6",
+    "url": "/static/js/2.1d8a223f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.1d8a223f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ada35c6262640fc403",
+    "url": "/static/js/main.a9375761.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
